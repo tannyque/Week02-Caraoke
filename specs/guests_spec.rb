1e75cc71a34c254321cdb165d8c00c3d888a5a79
tannyque <tannyque1992@gmail.com>
2018-07-28 19:55:57 +0100

    Adds money to guests

--- specs/guests_spec.rb
+++ specs/guests_spec.rb
@@ -5,11 +5,15 @@ require_relative("../guests")
 class GuestsTest < MiniTest::Test
 
   def setup()
-    @guest1 = Guest.new("Pim")
+    @guest1 = Guest.new("Pim", 7)
   end
 
   def test_guest_has_name()
     assert_equal("Pim", @guest1.name())
+  end
+
+  def test_guest_has_money()
+    assert_equal(7, @guest1.money())
   end
 
 end
